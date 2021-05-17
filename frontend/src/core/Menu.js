261f22bb366e15c4d96869b68718737bc8b88abf
boluwatifeajayi <bolu4good@gmail.com>
2021-05-17 04:22:00 +0100

    unity

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -20,8 +20,7 @@ const Menu = ({history}) => (
         <div class="navy sticky-top">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
 
-            <a href="Boluwatife Ajayi Resume.pdf" target="_blank" class="brand" href="#"
-                >Resume</a>
+            
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
